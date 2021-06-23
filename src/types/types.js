33e89eb47e08137b7baf0621272f7f6a34cd8dbb
Joel Vargas <teslavargas18@gmail.com>
2021-06-22 23:32:28 -0300

    finished

---
+++ src/types/types.js
@@ -0,0 +1,26 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event] Logout event',
+
+    eventStartAddNew: '[event] Start add new', 
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Events loaded',
+
+    authCheckingFinish: '[auht] Finish checking login state',
+    authStartLogin: '[auht] Start login',
+    authLogin: '[auht] Login',
+    authStartRegister: '[auht] Start register',
+    authStartTokenRenew: '[auht] Start token renew',
+    authLogout: '[auht] Logout',
+
+
+}
